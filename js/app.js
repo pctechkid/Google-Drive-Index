cef536f0a4c9fcfebfb0fcb2dd90eba54cf89e59
pctechkid <omboyronnel@gmail.com>
2020-12-24 01:14:33 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -93,7 +93,12 @@ function nav(path) {
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-	</ul>
+      
+
+	var search_text = model.is_search_page ? (model.q || '') : '';
+	const isMobile = Os.isMobile;
+	var search_bar = `
+</ul>
 <form class="form-inline my-2 my-lg-0" method="get" action="/${cur}:search">
 <input class="form-control mr-sm-2" name="q" type="search" placeholder="Search" aria-label="Search" value="${search_text}" required>
 <button class="btn ${UI.dark_mode ? 'btn-secondary' : 'btn-outline-success'} my-2 my-sm-0" onclick="if($('#search_bar').hasClass('mdui-textfield-expanded') && $('#search_bar_form>input').val()) $('#search_bar_form').submit();" type="submit">Search</button>
