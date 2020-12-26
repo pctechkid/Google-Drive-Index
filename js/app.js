e08220ce56c68fa704c2458ab2e21bf693e63634
pctechkid <omboyronnel@gmail.com>
2020-12-27 03:37:06 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -652,7 +652,7 @@ function file_video(path) {
 	const mx = url.replace(/\%20/g,' ');
 	const content = `
   <link href="https://cdn.jsdelivr.net/gh/jscdn/css@master/video-js/video-js.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/gh/pctechkid/Google-Drive-Index@master/js/fa.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/fontawesome.min.js" integrity="sha512-kI12xOdWTh/nL2vIx5Yf3z/kJSmY+nvdTXP2ARhepM/YGcmo/lmRGRttI3Da8FXLDw0Y9hRAyZ5JFO3NrCvvXA==" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/gh/jscdn/js@master/video-js/video-js.min.js"></script>
   <div class="container"><br>
   <div class="card">
