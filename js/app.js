a9de7d0755e8dbdb88bbbbb4c06abb880e4af248
pctechkid <omboyronnel@gmail.com>
2021-06-03 16:44:31 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -660,13 +660,15 @@ function file_video(path) {
   </div>
   	
   
-<br>
   </div>
+  <br>
+  <br>
+  <br>
   <div class="card">
   <div class="btn-group-vertical" role="group">
-		  <a class="btn btn-success mx-auto" href="${dl}"><i class="fa fa-download"></i> &nbsp;   Download</a>
-		  <a class="btn btn-primary mx-auto" href="intent:${url}#Intent;package=com.mxtech.videoplayer.pro;B.secure_uri=true;end"><i class="fa fa-play-circle"></i> &nbsp;   MX Player Pro</a>
-		  <a class="btn btn-danger mx-auto" href="vlc://${vlc}"><i class="fa fa-play-circle"></i> &nbsp;   VLC Media Player</a>
+		  <a class="btn btn-dark mx-auto" href="${dl}"><i class="fa fa-download"></i> &nbsp;   Download</a>
+		  <a class="btn btn-dark mx-auto" href="intent:${url}#Intent;package=com.mxtech.videoplayer.pro;B.secure_uri=true;end"><i class="fa fa-play-circle"></i> &nbsp;   MX Player Pro</a>
+		  <a class="btn btn-dark mx-auto" href="vlc://${vlc}"><i class="fa fa-play-circle"></i> &nbsp;   VLC Media Player</a>
   </div>
   </div>
   </div>
